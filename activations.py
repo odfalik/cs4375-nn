8ea683bac65970b7a16677de3f51e034c889a40e
Oded Falik <falik.oded.1@gmail.com>
2020-06-19 01:09:06 -0500

    backprop start + activations

--- activations.py
+++ activations.py
@@ -1,18 +1,39 @@
 import numpy as np
 
 
-
-
+# Returns a lambda which calculates the derivative of the activation function for a given activation value
 def getActivationFDerivative(activation):
-    pass
+    if (activation == 'sigmoid'):
+        return lambda x: x * (np.ones(x.shape[0]) - x)
 
+    elif (activation == 'tanh'):
+        return lambda x: 1-np.square(np.tanh(x))
+
+    elif (activation == 'relu'):
+        return lambda x: (x>0)*1    # TODO double check
+    
+    elif (activation == 'identity'):
+        return lambda x: 1
+
+    else:
+        print(f'Unsupported activation function: {activation}')
+        exit(1)
+
+
+# Returns the respective activation function
 def getActivationF(activation):
     if (activation == 'sigmoid'):
-        return lambda x: 1/(1 + np.exp(-x)) 
+        return lambda x: 1/(1 + np.exp(-x))
+
     elif (activation == 'tanh'):
         return np.tanh
+
     elif (activation == 'relu'):
-        return lambda x: 
+        lambda x: x if x > 0 else 0
+
+    elif (activation == 'identity'):
+        return lambda x: x
+
     else:
         print(f'Unsupported activation function: {activation}')
         exit(1)
--- ann.py
+++ ann.py
@@ -6,37 +6,41 @@ class ANN(object):
     def __init__(self, dims, activation):
         self.dims = np.asarray(dims, dtype=np.ushort)   # (1, L) array of layer dimensions
         self.L = len(self.dims)                         # number of layers
-        self.W = np.asarray([None] * self.L)            # (1, L) array of weight matrices
-        self.B = np.asarray([None] * self.L)            # (1, L) array of biases
+        self.W = np.asarray([None] * (self.L-1))          # (1, L-1) array of weight matrices which are indexed by [to_index, from_index]
 
-        A = [None] * self.L 
+        N = [None] * self.L
         for l, dim in enumerate(self.dims):
-            A[l] = np.asarray([0] * dim)
-        self.A = np.asarray(A)                          # (1, L) array of activation arrays (1, dim)
+            N[l] = np.asarray([0] * dim)
+        self.Z = np.asarray(N)                          # (1, L) jagged array of net arrays         (1, dim)
+        self.A = np.asarray(N)                          # (1, L) jagged array of activation arrays  (1, dim)
+        self.D = np.asarray(N)                          # (1, L) jagged array of delta arrays       (1, dim)
+
+        self.B = [None] * self.L-1
+        for l, dim in enumerate(self.dims):
+            # self.b[l] = 
+            pass
+        self.B = np.asarray(N)                          # (1, L-1) jagged array of biases           (1, dim)
+
 
         self.activation = activation                    # string containing name of activation function: sigmoid/tanh/relu
         self.activation_f = activations.getActivationF(activation)
         self.activation_f_derivative = activations.getActivationFDerivative(activation)
 
 
-    def predict__(self, x_v):
-        W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
-        z1 = x.dot(W1) + b1
-        a1 = np.tanh(z1)
-        z2 = a1.dot(W2) + b2
-        exp_scores = np.exp(z2)
-        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-        return np.argmax(probs, axis=1)
-
     def predict(self, x_v):
         for l in range(self.L):
             if (l == 0):
-                self.A[l] = 
+                self.A[l] = x_v.reshape(-1, 1)
             else:
-                
-        
+                a = self.A[l - 1].T
+                x = self.W[l].T
+                b = self.B[0,l]
+                print(f'asdfasdf', x.shape, b.shape)
+                self.Z = (a @ x) + b
+
 
     def train(self, x_m, y_m, learning_rate=0.001):
+
         # Weight initialization
         for l in range(1, self.L):
             l_dim = self.dims[l]
@@ -46,17 +50,37 @@ class ANN(object):
             if (self.activation == 'sigmoid' or self.activation == 'relu'):
                 # Xavier initialization (for sigmoid and relu activations)
                 self.W[l] = np.random.randn(l_dim, prev_l_dim) * np.sqrt( 1 / prev_l_dim)
-                self.B[l] = np.random.randn(1,1) * np.sqrt(1 / prev_l_dim)
+                self.B[0,l] = np.random.randn() * np.sqrt(1 / prev_l_dim)
             else:
                 # He initialization (for tanh activations)
                 self.W[l] = np.random.randn(l_dim, prev_l_dim) * np.sqrt( 2 / prev_l_dim)
-                self.B[l] = np.random.randn(1,1) * np.sqrt(2 / prev_l_dim)
+                self.B[0,l] = np.random.randn() * np.sqrt(2 / prev_l_dim)
 
-        # Until convergence, for each training example
-        for sample_idx, sample_x_v in enumerate(x_m):
-            # Forward pass
-            self.predict(sample_x_v)
-            
+        # Until convergence
+        for epoch in range(1000):
+            print(f'Epoch {epoch}')
+
+            # For each training example
+            for sample_idx, sample_x_v in enumerate(x_m):
+
+                # Forward pass
+                self.predict(sample_x_v)
+
+                # Backpropagation
+                # Iterate layers in reverse (excluding the input layer)
+                for l in range(self.L-1, 0, -1):
+
+                    # Cache delta_j (error term) which represents sensitivity of Cost to unit's activation -- will be used when computing later downstream summations                    
+                    if (l == self.L-1):     # Output layer
+                        self.D[l] = ((y_m[sample_idx] - self.A[l]) * self.activation_f_derivative(self.A[l])).reshape(-1, 1)
+                    else:                   # Hidden layer
+                        print(self.activation_f_derivative(self.A[l]).shape, self.D[l+1].shape, self.W[l].shape)
+                        self.D[l] = self.activation_f_derivative(self.A[l]).reshape(-1, 1) * (self.W[l] @ self.D[l+1])
+                    
+
+
+
+
 
     def test(self):
         pass
